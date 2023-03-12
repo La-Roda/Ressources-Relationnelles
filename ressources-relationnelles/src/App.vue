ca31614785de4b0f594e0bf30c66eadecd44c36f
Christophe Tosello <pro.christophe.tosello@gmail.com>
2023-03-12 16:17:21 +0100

    Front

---
+++ ressources-relationnelles/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+  <v-app-bar color="#009C9B">
+    <headerComponent></headerComponent>
+  </v-app-bar>
+    <v-main>
+        <router-view/>
+    </v-main>
+  </v-app>
+</template>
+<style>
+
+</style>
+<script>
+import headerComponent from './components/header.vue';
+export default {
+  name: 'App',
+  components: {
+    headerComponent
+  },
+  data () {
+    return {
+      //
+    }
+  },
+}
+</script>
