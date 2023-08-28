cef3ed2b4f75fdad702c1f00889bda9a4b45d170
Adrien Varricchione <47208777+Shedhatch@users.noreply.github.com>
2023-08-28 16:07:26 +0200

    RR: Added a new title

--- ressources-relationnelles/view/src/components/login/loginComp.vue
+++ ressources-relationnelles/view/src/components/login/loginComp.vue
@@ -1,39 +1,55 @@
 <template>
-    <v-container class="login-card">
-        <v-card>
-            <div class="pa-3 d-flex flex-column">
-            <h2 class="mb-3">Se connecter</h2>
-                <v-text-field v-model="login" variant="outlined" class="text-input" label="Login" name="login"></v-text-field>
-                <v-text-field v-model="password" variant="outlined" type="password" class="text-input" label="Mot de passe" name="password"></v-text-field>
-                <v-btn color="#009C9B" class="rounded-pill" @click="initLogin">Se connecter</v-btn>
-            </div>
-        </v-card>
-    </v-container>
+  <v-container class="login-card">
+    <v-card>
+      <div class="pa-3 d-flex flex-column">
+        <h2 class="mb-3">Se connecter a l'application</h2>
+        <v-text-field
+          v-model="login"
+          variant="outlined"
+          class="text-input"
+          label="Login"
+          name="login"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          variant="outlined"
+          type="password"
+          class="text-input"
+          label="Mot de passe"
+          name="password"
+        ></v-text-field>
+        <v-btn color="#009C9B" class="rounded-pill" @click="initLogin"
+          >Se connecter</v-btn
+        >
+      </div>
+    </v-card>
+  </v-container>
 </template>
 
 <style>
 .login-card {
-    max-width: 500px;
+  max-width: 500px;
 }
 .text-input {
-   
 }
 </style>
 <script>
 export default {
-    data() {
-        return {
-            login: null,
-            password: null
-        }
+  data() {
+    return {
+      login: null,
+      password: null,
+    };
+  },
+  methods: {
+    async initLogin() {
+      // this.$emit('initLogin', this.login, this.password)
+      await this.$store.dispatch("login", {
+        email: this.login,
+        password: this.password,
+      });
+      this.$store.dispatch("getPosts");
     },
-    methods: {
-        async initLogin() {
-            // this.$emit('initLogin', this.login, this.password)
-            await this.$store.dispatch('login', { email: this.login, password: this.password });
-            this.$store.dispatch('getPosts');
-            
-        }
-        }
-}
+  },
+};
 </script>
